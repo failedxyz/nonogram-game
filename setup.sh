6d1f744dbe243435a11956502181996b99aec270
Michael Zhang <me@michaelz.xyz>
2016-10-17 15:04:01 -0500

    Initial.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Updating system..."
+apt-get -y update
+apt-get -y upgrade
+
+apt-get -y install python-dev python-pip libffi-dev
+apt-get -y install postgresql postgresql-contrib libpq-dev
+
+cd /vagrant
+pip install -r requirements.txt
